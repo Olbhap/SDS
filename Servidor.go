032817257e0ffb168ea57c142f7898f477834253
melpr <melpr91@gmail.com>
2015-05-11 18:48:17 +0200

    Modificado Servidor para hacer up o down

--- Servidor.go
+++ Servidor.go
@@ -20,15 +20,19 @@ package main
 import (
 	//"bufio"
 
-	//"bufio"
+	"bufio"
 	//"compress/flate"
+	"bytes"
+	"compress/gzip"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
 	"crypto/rsa"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	"os"
 )
@@ -57,6 +61,13 @@ type Msg struct {
 	Comando string
 	Tipo    string
 	Nombre  string
+}
+
+func listar() {
+	files, _ := ioutil.ReadDir("./")
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
 }
 
 // gestiona el modo servidor
@@ -124,7 +135,7 @@ func server() {
 			je = json.NewEncoder(aeswr)
 			jd = json.NewDecoder(aesrd)
 			var i string = ""
-			for i != "Salir"{
+			for i != "Salir" {
 				// envíamos un mensaje de HELLO (ejemplo)
 				je.Encode(&Msg{Usuario: "Servidor", Comando: "", Tipo: "t", Nombre: "./ej"})
 
@@ -132,23 +143,48 @@ func server() {
 				var m Msg
 				jd.Decode(&m)
 				fmt.Println(m)
-				i=m.Nombre
+				i = m.Nombre
 				if Comprobar(m) == true {
 					if m.Tipo == "f" {
 						sourceinfo, err := os.Stat(m.Nombre)
-						if err != nil {
+						if err == nil {
+							fmt.Println("pe2")
 
+							err = os.MkdirAll("servidor/"+m.Usuario, sourceinfo.Mode())
+							if err != nil {
+								fmt.Println("pe3")
+								fmt.Println(err)
+
+							}
+
+							//CopyDir(m.Nombre, "servidor/"+m.Usuario+"/"+m.Nombre)
+							listar()
+							if m.Comando == "up" {
+
+								CopyFile(m.Nombre, "servidor/"+m.Usuario+"/"+m.Nombre)
+							} else if m.Comando == "delete" {
+								os.Remove("servidor/" + m.Usuario + "/" + m.Nombre)
+							} else {
+								Comprimir("servidor/" + m.Usuario + "/" + m.Nombre)
+
+								CopyFile("servidor/"+m.Usuario+"/"+m.Nombre+".gz", m.Nombre+".gz")
+								os.Remove("servidor/" + m.Usuario + "/" + m.Nombre + ".gz")
+							}
 						}
-
-						err = os.MkdirAll("servidor/"+m.Usuario+"/", sourceinfo.Mode())
-						if err != nil {
-
-						}
-
-						//CopyDir(m.Nombre, "servidor/"+m.Usuario+"/"+m.Nombre)
-						CopyFile(m.Nombre, "servidor/"+m.Usuario+"/"+m.Nombre)
 					} else {
-						CopyDir(m.Nombre, "servidor/"+m.Usuario+"/"+m.Nombre)
+						listar()
+						if m.Comando == "up" {
+
+							CopyDir(m.Nombre, "servidor/"+m.Usuario+"/"+m.Nombre)
+						} else if m.Comando == "delete" {
+							//os.RemoveAll("servidor/" + m.Usuario + "/" + m.Nombre)
+						} else {
+							//Comprimir("servidor/" + m.Usuario + "/" + m.Nombre)
+
+							CopyDir("servidor/"+m.Usuario+"/"+m.Nombre, m.Nombre)
+							//os.Remove("servidor/" + m.Usuario + "/" + m.Nombre + ".gz")
+						}
+
 					}
 				}
 				//CopyFile(m.Id, "output.txt")
@@ -168,12 +204,65 @@ func server() {
 		}()
 	}
 }
+
+func Comprimir(nombre string) bool {
+	flag.Parse() // get the arguments from command line
+
+	filename := nombre
+
+	if filename == "" {
+		fmt.Println("Usage : go-gzip sourcefile")
+		os.Exit(1)
+	}
+
+	rawfile, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer rawfile.Close()
+
+	// calculate the buffer size for rawfile
+	info, _ := rawfile.Stat()
+
+	var size int64 = info.Size()
+	rawbytes := make([]byte, size)
+
+	// read rawfile content into buffer
+	buffer := bufio.NewReader(rawfile)
+	_, err = buffer.Read(rawbytes)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var buf bytes.Buffer
+	writer := gzip.NewWriter(&buf)
+	writer.Write(rawbytes)
+	writer.Close()
+
+	err = ioutil.WriteFile(filename+".gz", buf.Bytes(), info.Mode())
+	// use 0666 to replace info.Mode() if you prefer
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%s compressed to %s\n", filename, filename+".gz")
+	return true
+}
+
 func Comprobar(mensaje Msg) bool {
 	var comprobar = true
 	switch mensaje.Comando {
 	case "up":
 		comprobar = ComprobarTipo(mensaje)
 	case "down":
+		comprobar = ComprobarTipo(mensaje)
+	case "delete":
 		comprobar = ComprobarTipo(mensaje)
 	default:
 		fmt.Println("Comando incorrecto por favor introduzca up/down")
@@ -199,9 +288,11 @@ func ComprobarTipo(mensaje Msg) bool {
 
 //Copia ficheros crea el fichero destino
 func CopyFile(source string, dest string) (err error) {
-
+	fmt.Println("Copiando fichero...")
 	sourcefile, err := os.Open(source)
 	if err != nil {
+		fmt.Print("1 ")
+		fmt.Println(err)
 		return err
 	}
 
@@ -209,6 +300,8 @@ func CopyFile(source string, dest string) (err error) {
 
 	destfile, err := os.Create(dest)
 	if err != nil {
+		fmt.Print("2 ")
+		fmt.Println(err)
 		return err
 	}
 
@@ -218,7 +311,10 @@ func CopyFile(source string, dest string) (err error) {
 	if err == nil {
 		sourceinfo, err := os.Stat(source)
 		if err != nil {
+
 			err = os.Chmod(dest, sourceinfo.Mode())
+			fmt.Print("3 ")
+			fmt.Println(err)
 		}
 
 	}
@@ -229,6 +325,7 @@ func CopyFile(source string, dest string) (err error) {
 //Copia todo el contenido de la carpeta indicada incluso las carpetas que hay dentro
 func CopyDir(source string, dest string) (err error) {
 
+	fmt.Println("Copiando directorio..." + source + " - " + dest)
 	sourceinfo, err := os.Stat(source)
 	if err != nil {
 		return err
